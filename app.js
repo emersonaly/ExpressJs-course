6afebc618ac73ed0133df9d38b21f58f98a483b1
Emerson Ramirez <ramirezemerson1991@gmail.com>
2026-04-24 18:07:28 -0400

    init project

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    `
+    <h1>Curso de ExpressJS</h1>
+    <p>Aplicaciones web dinamicas con express</p>
+    <p>Corre en el puerto: ${PORT}</p>
+    `
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
