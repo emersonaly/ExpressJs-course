3eb82dbdaa7356b33a4ecc0dbdf8f6d004b5a523
Emerson Ramirez <ramirezemerson1991@gmail.com>
2026-04-24 18:37:50 -0400

    Example of retrieving parameters

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send(
@@ -13,6 +17,22 @@ app.get("/", (req, res) => {
     `
   );
 });
+
+app.get('/users/:id', (req,res) => {
+  const userId = req.params.id; // obtenemos el parametro de la ruta :id
+
+  res.send(`Usuario encontrado: ${userId}`)
+})
+
+app.get('/search', (req, res) => {
+  // obtenemos los parametros de la consulta 
+  const term = req.query.term || 'no envio termino de busqueda'
+  const category = req.query.category || 'no envio categoria'
+  
+  res.send(`<h2>Resultados de la busqueda</h2> 
+    <p>Busqueda: <strong>${term}</strong></p> 
+    <p>Categoria: <strong>${category}</strong></p> `)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
