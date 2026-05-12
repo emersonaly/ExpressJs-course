344b72e73b863ad30929661aaf24a722ddb427b5
Emerson Ramirez <ramirezemerson1991@gmail.com>
2026-05-12 10:23:01 -0400

    feat: implement Prisma database integration and service layer for authentication, reservations, and appointments

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,6 @@
 import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs";
-import { PrismaClient } from '../../generated/client/index.js';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+import prisma from '../db.js';
 
 export const registerUser = async (name, email, password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
